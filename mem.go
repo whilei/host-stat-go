5cc210b80d1c053ae32fd9082c11115fc6ac2d77
ia <isaac.ardis@gmail.com>
2018-06-17 00:59:38 +0200

    all: gofmt
    Run standard gofmt command on project root.
    
    - go version go1.10.3 darwin/amd64
    
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- mem.go
+++ mem.go
@@ -9,76 +9,72 @@
 
 package host_stat
 
-
 import (
-    "strings"
-    "strconv"
-    "io/ioutil"
+	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
-
 type MemStat struct {
-    MemTotal  uint64  `json:"mem_total"`
-    MemUsed   uint64  `json:"mem_used"`
-    MemFree   uint64  `json:"mem_free"`
-    Buffers   uint64  `json:"buffers"`
-    Cached    uint64  `json:"cached"`
-    SwapTotal uint64  `json:"swap_total"`
-    SwapUsed  uint64  `json:"swap_used"`
-    SwapFree  uint64  `json:"swap_free"`
-    MemRate   float64 `json:"mem_rate"`
-    SwapRate  float64 `json:"swap_rate"`
+	MemTotal  uint64  `json:"mem_total"`
+	MemUsed   uint64  `json:"mem_used"`
+	MemFree   uint64  `json:"mem_free"`
+	Buffers   uint64  `json:"buffers"`
+	Cached    uint64  `json:"cached"`
+	SwapTotal uint64  `json:"swap_total"`
+	SwapUsed  uint64  `json:"swap_used"`
+	SwapFree  uint64  `json:"swap_free"`
+	MemRate   float64 `json:"mem_rate"`
+	SwapRate  float64 `json:"swap_rate"`
 }
-
 
 func GetMemStat() (stat MemStat, err error) {
-    text, err := ioutil.ReadFile("/proc/meminfo")
-    if err != nil {
-        return
-    }
+	text, err := ioutil.ReadFile("/proc/meminfo")
+	if err != nil {
+		return
+	}
 
-    stat = MemStat{}
-    lines := strings.Split(string(text), "\n")
-    for i:=0; i<len(lines); i++ {
-        if !strings.Contains(lines[i], ":") {
-            continue
-        }
+	stat = MemStat{}
+	lines := strings.Split(string(text), "\n")
+	for i := 0; i < len(lines); i++ {
+		if !strings.Contains(lines[i], ":") {
+			continue
+		}
 
-        maps := strings.Split(lines[i], ":")
-        key := strings.Trim(maps[0], " ")
-        if key == "MemTotal" {
-            stat.MemTotal = parse_mem_value(maps[1])
-        } else if key == "MemFree" {
-            stat.MemFree = parse_mem_value(maps[1])
-        } else if key == "Buffers" {
-            stat.Buffers = parse_mem_value(maps[1])
-        } else if key == "Cached" {
-            stat.Cached = parse_mem_value(maps[1])
-        } else if key == "SwapTotal" {
-            stat.SwapTotal = parse_mem_value(maps[1])
-        } else if key == "SwapFree" {
-            stat.SwapFree = parse_mem_value(maps[1])
-        }
-    }
+		maps := strings.Split(lines[i], ":")
+		key := strings.Trim(maps[0], " ")
+		if key == "MemTotal" {
+			stat.MemTotal = parse_mem_value(maps[1])
+		} else if key == "MemFree" {
+			stat.MemFree = parse_mem_value(maps[1])
+		} else if key == "Buffers" {
+			stat.Buffers = parse_mem_value(maps[1])
+		} else if key == "Cached" {
+			stat.Cached = parse_mem_value(maps[1])
+		} else if key == "SwapTotal" {
+			stat.SwapTotal = parse_mem_value(maps[1])
+		} else if key == "SwapFree" {
+			stat.SwapFree = parse_mem_value(maps[1])
+		}
+	}
 
-    stat.MemUsed = stat.MemTotal - stat.MemFree
-    stat.SwapUsed = stat.SwapTotal - stat.SwapFree
-    if stat.MemTotal > 0 {
-        stat.MemRate = Round(float64(stat.MemUsed) * 100 / float64(stat.MemTotal), 2)
-    }
-    if stat.SwapTotal > 0 {
-        stat.SwapRate = Round(float64(stat.SwapUsed) * 100 / float64(stat.SwapTotal), 2)
-    }
+	stat.MemUsed = stat.MemTotal - stat.MemFree
+	stat.SwapUsed = stat.SwapTotal - stat.SwapFree
+	if stat.MemTotal > 0 {
+		stat.MemRate = Round(float64(stat.MemUsed)*100/float64(stat.MemTotal), 2)
+	}
+	if stat.SwapTotal > 0 {
+		stat.SwapRate = Round(float64(stat.SwapUsed)*100/float64(stat.SwapTotal), 2)
+	}
 
-    return
+	return
 }
 
-
 func parse_mem_value(value string) (mem uint64) {
-    data := strings.Fields(value)
+	data := strings.Fields(value)
 
-    mem, _ = strconv.ParseUint(strings.Trim(data[0], " "), 10, strconv.IntSize)
-    mem = mem / 1024
+	mem, _ = strconv.ParseUint(strings.Trim(data[0], " "), 10, strconv.IntSize)
+	mem = mem / 1024
 
-    return
+	return
 }
--- utils.go
+++ utils.go
@@ -9,58 +9,53 @@
 
 package host_stat
 
-
-import(
-    "os"
-    "math"
-    "strings"
-    "io/ioutil"
+import (
+	"io/ioutil"
+	"math"
+	"os"
+	"strings"
 )
 
-
 func Round(data float64, precision int) (result float64) {
-    pow := math.Pow(10, float64(precision))
-    digit := pow * data
-    _, div := math.Modf(digit)
+	pow := math.Pow(10, float64(precision))
+	digit := pow * data
+	_, div := math.Modf(digit)
 
-    if div >= 0.5 {
-        result = math.Ceil(digit)
-    } else {
-        result = math.Floor(digit)
-    }
-    result = result / pow
+	if div >= 0.5 {
+		result = math.Ceil(digit)
+	} else {
+		result = math.Floor(digit)
+	}
+	result = result / pow
 
-    return
+	return
 }
 
-
-func IsFileExists(fname string) (bool) {
-    if _, err := os.Stat(fname); err == nil {
-        return true
-    }
-    return false
+func IsFileExists(fname string) bool {
+	if _, err := os.Stat(fname); err == nil {
+		return true
+	}
+	return false
 }
-
 
 func ReadFirstLine(fname string) (line string, err error) {
-    text, err := ReadFile(fname)
-    if err != nil {
-        return
-    }
+	text, err := ReadFile(fname)
+	if err != nil {
+		return
+	}
 
-    lines := strings.Split(string(text), "\n")
-    line = strings.Trim(lines[0], " ")
+	lines := strings.Split(string(text), "\n")
+	line = strings.Trim(lines[0], " ")
 
-    return
+	return
 }
 
-
 func ReadFile(fname string) (result string, err error) {
-    text, err := ioutil.ReadFile(fname)
-    if err != nil {
-        return
-    }
-    result = string(text)
+	text, err := ioutil.ReadFile(fname)
+	if err != nil {
+		return
+	}
+	result = string(text)
 
-    return
+	return
 }
